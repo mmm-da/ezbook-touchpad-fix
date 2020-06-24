18884d503cab910a056c2a1aaaa064497852f1dc
spAm25 <39336091+spAm25@users.noreply.github.com>
2020-06-24 14:53:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cp ./touchpad-fix/touchpad-fix /usr/local/bin
 
 chmod +x /usr/local/bin/touchpad-fix
 
-sudo systemctl enable touchpad-fix
-sudo systemctl start touchpad-fix
+systemctl enable touchpad-fix
+systemctl start touchpad-fix
